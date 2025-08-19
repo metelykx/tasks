e44764aca132da215bd9dacc74731db7c3b3eaa7
metelykx <ivaschenkoodenys@gmail.com>
2025-08-19 22:44:09 +0300

    33 fin

---
+++ kata_8/33.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+enum Chi: Int {
+    case one = 5
+    case two = 10
+    case three = 15
+    case four = 20
+    case five = 25
+    case six = 30
+    case seven = 35
+    case eight = 40
+    case nine = 45
+    case ten = 50
+}
