a9f62221777b23f2edd1c890cead0bf3f8040d14
metelykx <ivaschenkoodenys@gmail.com>
2025-08-19 22:27:16 +0300

    12

---
+++ kata_8/12.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+enum Chislo: Int {
+    case One = 1
+    case Two = 2
+    case Three = 3
+    case Four = 4
+    case Five = 5
+    case Six = 6
+    case Seven = 7
+    case Eight = 8
+    case Nine = 9
+    case Ten = 10
+}
+
+
+var a = Chislo.One.rawValue
+print(a)
